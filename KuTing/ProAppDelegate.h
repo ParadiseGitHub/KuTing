1d9c798f54a2a27202b364b40d28e8116a5210c2
ParadiseGitHub <823115753@qq.com>
2014-05-27 08:40:47 +0800

    Initial Commit

---
+++ KuTing/ProAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  ProAppDelegate.h
+//  KuTing
+//
+//  Created by jmy on 14-5-27.
+//  Copyright (c) 2014年 jmy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ProAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ KuTing/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  KuTing
+//
+//  Created by jmy on 14-5-27.
+//  Copyright (c) 2014年 jmy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "ProAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([ProAppDelegate class]));
+    }
+}
